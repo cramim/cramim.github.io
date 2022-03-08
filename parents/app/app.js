9c4a28edfe32b309f4f549147c64bd1a74458f1c
cramim <cramim.community@gmail.com>
2022-03-09 00:58:21 +0200

    get activity sheet data

--- parents/app/app.js
+++ parents/app/app.js
@@ -1,25 +1,62 @@
-﻿var app = {
+﻿var js = {
+    clean_google_sheet_array:(array, remove_header_row, null_empty_strings)=>{
+        if (remove_header_row) array.shift();
+        if (null_empty_strings) {
+            $.each(array, (i, item)=>{
+                $.each(item, (ii, field)=>{
+                    if ("" === (""+field).trim()) array[i][ii] = null;
+                })
+            });
+        }
+    }
+};
+
+var app = {
+    dat:{
+        activity_list:[]
+    },
     is_mobile: false,
-    get_sheet_data: ()=>{
+    get: (content_id, on_success, on_failure)=>{
         $.ajax({
             type: 'GET',
             url: 'https://script.google.com/macros/s/AKfycbw9iVdg5gGV0OzF_KOMDVjFZkL-HrIMd9vjiF7vsO-1dNcs0eRx_L5-g9D4rMEMGDjMTQ/exec',
-            // contentType: 'application/x-www-form-urlencoded; charset=utf-8',
             accept: 'application/json',
             contentType: 'text/plain',                    
             async:true,
             //timeout: 10000,
             cache: false,
             // dataType:'json',
-            // data:JSON.stringify(json_post_data),
-            success: (response)=>{
-                $("#mm_test").val(response);
-                console.log(response);
+            data:{content_id:content_id},
+            success:(response)=>{
+                if (response?.error){
+                    console.error("app.get.success", `content_id="${content_id}"`, response.error);
+                } else {
+                    if (on_success) on_success(response);
+                }
             },
             error: (jqXHR, textStatus, errorThrown)=> {
+                console.error("app.get.error", `content_id="${content_id}"`, jqXHR, textStatus, errorThrown);
             }
         });
-        console.log("get_sheet_data");
+    },
+    load:()=>{
+        app.get("activity", (response)=>{
+            js.clean_google_sheet_array(response, true, true);
+            $.each(response, (i, item)=>{app.dat.activity_list.push({
+                id: item[0],
+                activity: item[1],
+                group: item[2],
+                description: item[3],
+                hours: item[4],
+                category: item[5],
+                circle: item[6],
+                timing: item[7],
+                members_goal: item[8],
+                members_minimum: item[9],
+                mambers_maximum: item[10]
+            })});
+            console.log(app.dat.activity_list);
+        });
     },
     progress_bar:{
         write:($container)=>{
@@ -54,14 +91,11 @@
             $(ev.target).closest(".filter_box_item").find("input").click();
         });
     },
-    init_mobile(){
+    init_mobile: ()=>{
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) app.is_mobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
-        console.log(app.is_mobile)
+        console.log((app.is_mobile)?"mobile device detected":"no mobile device detected");
     },
-    init: ()=>{
-        app.init_mobile();
-        app.init_scroll();
-        app.init_buttons();
+    init_demo: ()=>{
         app.progress_bar.write($("#user_box_progress"));
         app.progress_bar.set_progress($("#user_box_progress"), 50);
 
@@ -69,10 +103,15 @@
             app.progress_bar.write($(el));
             app.progress_bar.set_progress($(el), Math.floor(Math.random() * 101));
         });
-        
-        // app.progress_bar.set_progress($("#dv_progress_test"));
+    },
+    init: ()=>{
+        console.log("app.init")
+        app.init_mobile();
+        app.init_scroll();
+        app.init_buttons();
+        app.init_demo();
+        app.load();
         $("#bt_test").click(app.get_sheet_data);
-        console.log("init");
     }
 }
 
