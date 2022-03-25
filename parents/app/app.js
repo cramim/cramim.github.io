e75e51bd0935b0d09d14b81fe0d31cb7cddccbed
cramim <cramim.community@gmail.com>
2022-03-25 22:30:38 +0300

    user mode + activity group + removed demo

--- parents/app/app.js
+++ parents/app/app.js
@@ -11,8 +11,12 @@ var js = {
         var is_mobile = false;
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) is_mobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
         return is_mobile;
-    }
-}
+    },
+    urlParam:name=>{
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        if (!results) return null;
+        return decodeURI(results[1]) || 0;
+    }}
 
 var app = {
     dat:{
@@ -24,8 +28,10 @@ var app = {
         user_goal:0,
         initial_signup_list:[],
         mandatory_activity_list:[],
+        mode:null,
         idx:{
-            activity_list:{}
+            activity_list:{},
+            activity_groups:{}
         }
     },
     is_mobile: false,
@@ -105,20 +111,32 @@ var app = {
             const activity = {
                 id: item[0],
                 name: item[1],
-                group: item[2],
+                parent: item[2],
                 description: item[3],
                 hours: item[4],
                 category: item[5],
                 circle: item[6],
                 timing: item[7],
-                members_goal: item[8],
+                members_goal: item[8] || 0,
                 members_minimum: item[9],
                 mambers_maximum: item[10],
-                signedup: item[11],
+                signedup: item[11] || 0,
                 mandatory: item[12],
-                locked: item[13]
+                locked: item[13],
+                group_name: item[14]
             };
-            app.dat.activity_list.push(activity)
+            if (activity.group_name) {
+                activity_group = app.dat.idx.activity_groups[activity.group_name];
+                if (activity_group) {
+                    activity_group.members_goal += activity.members_goal;
+                    activity_group.signedup += activity.signedup;
+                    activity_group.mambers_maximum += activity.mambers_maximum;
+                    return true; // JQuery continue each loop
+                }
+                app.dat.idx.activity_groups[activity.group_name] = activity;
+                activity.name = activity.group_name;
+            }
+            app.dat.activity_list.push(activity);
             app.dat.idx.activity_list[activity.id] = activity;
             if (activity.mandatory) app.dat.mandatory_activity_list.push(activity.id);
         });
@@ -137,11 +155,11 @@ var app = {
                     `<div class="activity_box_desc">${item.description||'&nbsp'}</div>` +
                     `<div class="activity_box_status"><table><tr>` +
                         `<td class="activity_box_status_value_title">שעות:</td><td class="activity_box_status_value">${item.hours||'&nbsp'}</td>` +
-                        `<td class="activity_box_status_joined_title">הצטרפו:</td><td class="activity_box_status_joined">${item.signedup||0}</td>` +
+                        `<td class="activity_box_status_joined_title">הצטרפו:</td><td class="activity_box_status_joined">${item.signedup}</td>` +
                         `<td class="activity_box_status_goal_title">מתוך:</td><td class="activity_box_status_goal">${item.members_goal||'&nbsp'}</td>` +
                     `</tr></table></div>` +
                     `<div class="activity_box_toolbox">` +
-                        `<div id="dv_activity_progress_${item.id}" progress="${(item.members_goal)?100*(item.signedup||0)/item.members_goal:'&nbsp'}" class="dv_activity_progress"></div>` +
+                        `<div id="dv_activity_progress_${item.id}" progress="${(item.members_goal)?100*(item.signedup)/item.members_goal:'&nbsp'}" class="dv_activity_progress"></div>` +
                         `<input class="bt_activity_add" type="button" value="הצטרפ/י">` +
                     `</div>`+
                 `</div>`;
@@ -242,6 +260,7 @@ var app = {
         $("#filter_box_wrapper input[type='checkbox']").prop("checked", false);
         app.dat.activity_list = [];
         app.dat.idx.activity_list = {};
+        app.dat.idx.activity_groups = {};
         app.dat.user = {};
         app.dat.signup_list = [];
         app.dat.initial_signup_list = [];
@@ -451,17 +470,15 @@ var app = {
         app.dat.user = window.localStorage.getObj("cramim-parents-user");
         console.log("app.init_user", app.dat.user);
     },
-    init_demo: ()=>{
-        app.progress_bar($("#user_box_progress"), 50);
-        app.dat.user = {
-            uid: "menikupfer@gmail.com",
-            name: "משפחת קופפר"
-        };
-        console.log("app.init_demo user:", app.dat.user);
-    },
-    show_screen_message(msg){
+    show_screen_message: msg=>{
         $("#dv_screen_message").fadeIn();
         $("#dv_screen_message_txt").html(msg);
+    },
+    init_mode: ()=> {
+        app.dat.mode = js.urlParam("mode");
+        console.log("mode=", app.dat.mode);
+        $("body").addClass("mode_" + app.dat.mode);
+        if (app.dat.mode) $("#ttl_mode").html(app.dat.mode).show();
     },
     init: ()=>{
         console.log("app.init")
@@ -471,7 +488,7 @@ var app = {
         app.init_scroll();
         app.init_buttons();
         app.init_user();
-        // app.init_demo();
+        app.init_mode();
         if (app.dat.user) {
             app.login(app.dat.user.uid, ()=>{app.show_screen_message("אין תקשורת עם השרת :(")});
         } else {
