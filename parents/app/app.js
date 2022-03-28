442909afe478e2b0f32ebe300d5e1269c183f392
cramim <cramim.community@gmail.com>
2022-03-28 23:12:00 +0300

    mobile menu

--- parents/app/app.js
+++ parents/app/app.js
@@ -303,12 +303,12 @@ var app = {
                 '<div class="help_paragraph">ברוכים הבאים לממשק ההרשמה למעורבות ההורים בכרמים. מוזמנים להירשם לפעילויות בהן תרצו להשתלב. שימו לב, ההרשמה הינה לרבעון הקרוב והיא משותפת לזוג ההורים.</div>' +
                 '<div class="help_paragraph">מצאו פעילויות ולחצו על הכפתור "הצטרפ/י".<br>הפעילויות אליהן הצטרפתם נאספות ומופיעות בצדו השמאלי של המסך.</div>' +
                 '<div class="help_paragraph">העזרו באפשרויות הסינון שבראש הדף כדי למצוא פעילויות לרוחכם.</div>' +
-                '<div class="help_nagging"><input id="cb_help_welcome_nagging" type="checkbox"/><label for="cb_help_welcome_nagging">הבנתי, אין צורך להציג הודעה זו שוב.</label></div>' +
+                '<div class="help_nagging"><input id="cb_help_welcome_nagging" type="checkbox" checked/><label for="cb_help_welcome_nagging">הבנתי, אין צורך להציג הודעה זו שוב.</label></div>' +
             '</div>',
         signup:'<div id="help_signup">' + 
             '<div class="help_paragraph">המשיכו לחפש ולהצטרף לפעילויות נוספות, ולסיום לחצו "שמירה".</div>' +
                 '<div class="help_paragraph">בכל שלב (גם לאחר השמירה) ניתן להסיר ההצטרפות ע"י לחיצה על צלמית הפח, המופיעה במעבר העכבר, מעל כל פעילות ברשימה שלכם בצד שמאל.</div>' + 
-                '<div class="help_nagging"><input id="cb_help_signup_nagging" type="checkbox"/><label for="cb_help_signup_nagging">הבנתי, אין צורך להציג הודעה זו שוב.</label></div>' +
+                '<div class="help_nagging"><input id="cb_help_signup_nagging" type="checkbox" checked/><label for="cb_help_signup_nagging">הבנתי, אין צורך להציג הודעה זו שוב.</label></div>' +
             '</div'
     },
     help_message:{
