6ddfecc683256cb06600af73f3257625b04733df
cramim <cramim.community@gmail.com>
2022-03-29 14:53:44 +0300

    github que

--- parents/app/app.js
+++ parents/app/app.js
@@ -1,4 +1,5 @@
-﻿Storage.prototype.setObj = function(key, value) {this.setItem(key, JSON.stringify(value));}
+﻿//app.js
+Storage.prototype.setObj = function(key, value) {this.setItem(key, JSON.stringify(value));}
 Storage.prototype.getObj = function(key) {var value = this.getItem(key); return value && JSON.parse(value); }
 
 var js = {
